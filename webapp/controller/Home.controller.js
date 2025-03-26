597c06ed5d591ad94a02846cbb11f3dc0eb002e6
Rafael Arias <ariasnavarrorafael@gmail.com>
2025-03-26 20:27:22 +0000

    initial set up

---
+++ webapp/controller/Home.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    'sap/ui/model/Filter',
+    'sap/ui/model/FilterOperator',
+],
+    function (Controller, Filter, FilterOperator) {
+        "use strict";
+
+        return Controller.extend("com.bootcamp.sapui5.smartcontrols1.controller.Home", {
+            onInit: function () {
+
+            },
+            onBeforeRebind: function (oEvent) {
+                let oParameters = oEvent.getParameter("bindingParams");
+                oParameters.filters.push(new Filter("IsActiveEntity", FilterOperator.EQ, true));
+            },
+        });
+    });
